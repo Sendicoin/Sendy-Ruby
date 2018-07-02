ae04d91380e3eed944b96dcd659b498c7e894e31
aidasv <aidasv@gmail.com>
2018-07-02 13:39:44 +0300

    extracting events

---
+++ lib/sendy/events.rb
@@ -0,0 +1,15 @@
+module Sendy
+  class Events
+    def events_count
+      api_call('get', EVENTS_COUNT_URL)['count']
+    end
+
+    def create_event(params)
+      api_call('post', EVENTS_URL, params)
+    end
+
+    def events
+      api_call('get', EVENTS_URL).map { |event| OpenStruct.new(event) }
+    end
+  end
+end
