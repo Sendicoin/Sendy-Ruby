c15976a4b778fd4877c6c8bec59959b24149d7a8
aidasv <aidasv@gmail.com>
2018-07-04 14:25:09 +0300

    moving constants to main modul

--- lib/sendy.rb
+++ lib/sendy.rb
@@ -23,6 +23,19 @@ require "sendy/events"
 require "sendy/transactions"
 require "sendy/users"
 
+SENDY_HOST = @api_base
+LOGIN_URL = "#{SENDY_HOST}/auth/login".freeze
+SUBSCRIBERS_URL = "#{SENDY_HOST}/api/subscribers".freeze
+EVENTS_URL = "#{SENDY_HOST}/api/events".freeze
+CAMPAIGNS_URL = "#{SENDY_HOST}/api/campaigns".freeze
+CAMPAIGNS_COUNT_URL = "#{SENDY_HOST}/api/campaigns/count.json".freeze
+SUBSCRIBERS_COUNT_URL = "#{SENDY_HOST}/api/subscribers/count.json".freeze
+EVENTS_COUNT_URL = "#{SENDY_HOST}/api/events/count.json".freeze
+TRANSACTIONS_URL = "#{SENDY_HOST}/api/transactions".freeze
+CREATE_USER_URL = "#{SENDY_HOST}/auth/signup"
+FIND_USER_URL = "#{SENDY_HOST}/auth/find_user"
+ADD_TOKENS_URL = "#{SENDY_HOST}/api/add_tokens_to_user"
+
 module Sendy
   @app_info = nil
 end
