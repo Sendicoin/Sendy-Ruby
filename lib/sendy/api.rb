ed94c4e515be39216383dc97c76ca32616ecaec3
aidasv <aidasv@gmail.com>
2018-07-10 11:01:44 +0300

    including methods into api class

--- lib/sendy/api.rb
+++ lib/sendy/api.rb
@@ -1,6 +1,11 @@
 module Sendy
   class Api
     include Sendy
+    include User
+    include Transaction
+    include Event
+    include Campaign
+    include Subscriber
 
     attr_accessor :email, :password, :esp_id, :authorization
 
