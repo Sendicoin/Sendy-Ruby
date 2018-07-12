11e471b2f71d6d30ea94fbfa767f35c94118d011
Audrius Brazauskas <audriustbrazauskas@gmail.com>
2018-07-12 14:04:13 +0300

    using attr_accessor

--- lib/sendy.rb
+++ lib/sendy.rb
@@ -24,23 +24,21 @@ require "sendy/transaction"
 require "sendy/user"
 
 module Sendy
+  attr_accessor :app_host, :app_esp_name, :app_esp_password
+
   @app_info = nil
 
-  @app_host = nil
-  @app_esp_name = nil
-  @app_esp_password = nil
-
-  LOGIN_URL = "#{@app_host}/auth/login".freeze
-  SUBSCRIBERS_URL = "#{@app_host}/api/subscribers".freeze
-  EVENTS_URL = "#{@app_host}/api/events".freeze
-  CAMPAIGNS_URL = "#{@app_host}/api/campaigns".freeze
-  CAMPAIGNS_COUNT_URL = "#{@app_host}/api/campaigns/count.json".freeze
-  SUBSCRIBERS_COUNT_URL = "#{@app_host}/api/subscribers/count.json".freeze
-  EVENTS_COUNT_URL = "#{@app_host}/api/events/count.json".freeze
-  TRANSACTIONS_URL = "#{@app_host}/api/transactions".freeze
-  CREATE_USER_URL = "#{@app_host}/auth/signup"
-  FIND_USER_URL = "#{@app_host}/auth/find_user"
-  ADD_TOKENS_URL = "#{@app_host}/api/add_tokens_to_user"
+  LOGIN_URL = "#{app_host}/auth/login".freeze
+  SUBSCRIBERS_URL = "#{app_host}/api/subscribers".freeze
+  EVENTS_URL = "#{app_host}/api/events".freeze
+  CAMPAIGNS_URL = "#{app_host}/api/campaigns".freeze
+  CAMPAIGNS_COUNT_URL = "#{app_host}/api/campaigns/count.json".freeze
+  SUBSCRIBERS_COUNT_URL = "#{app_host}/api/subscribers/count.json".freeze
+  EVENTS_COUNT_URL = "#{app_host}/api/events/count.json".freeze
+  TRANSACTIONS_URL = "#{app_host}/api/transactions".freeze
+  CREATE_USER_URL = "#{app_host}/auth/signup"
+  FIND_USER_URL = "#{app_host}/auth/find_user"
+  ADD_TOKENS_URL = "#{app_host}/api/add_tokens_to_user"
 
   class DuplicateEvent < StandardError; end
   class InvalidParams < StandardError; end
@@ -72,7 +70,7 @@ module Sendy
   end
 
   def self.esp_login_params
-    { esp_name: @app_esp_name, esp_password: @app_esp_password }
+    { esp_name: app_esp_name, esp_password: app_esp_password }
   end
 
   def relogin
