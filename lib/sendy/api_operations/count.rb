a4098c27daa27b0dac6a298918094797e3b4fbac
Marcos Teixeira <m.viniteixeira@gmail.com>
2018-08-24 16:22:11 -0300

    Passing params to count

--- lib/sendy/api_operations/count.rb
+++ lib/sendy/api_operations/count.rb
@@ -3,8 +3,8 @@
 module Sendy
   module APIOperations
     module Count
-      def count
-        resp = request(:get, resource_count_url)
+      def count(params = {})
+        resp = request(:get, resource_count_url, params)
         resp.data[:count]
       end
     end
