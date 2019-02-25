60ad40c3df41a7e2c8e4794e55154b3fa9004d2d
marius <pakalnis.marius@gmail.com>
2019-02-25 17:02:09 +0200

    Adding transaction method to accept additional params

--- lib/sendy/transaction.rb
+++ lib/sendy/transaction.rb
@@ -1,19 +1,18 @@
 module Sendy
   module Transaction
-
-    def transactions
-      api_call('get', transactions_url).map do |transaction| 
+    def transactions(args={})
+      api_call('get', transactions_url(args)).map do |transaction|
         Transaction.new(OpenStruct.new(transaction))
       end
     end
 
-    def transactions_url
-      "#{Sendy.app_host}/api/transactions"
+    def transactions_url(params={})
+      "#{Sendy.app_host}/api/transactions?#{params.to_query}"
     end
 
     class Transaction
       include Sendy
-      attr_reader :type, :created_at, :update_at, :amount
+      attr_reader :type, :created_at, :update_at, :amount, :moment_balance
 
       def initialize(params)
         @type = params[:type]
